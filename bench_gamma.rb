051bcd688c3a5de792bd5b47ae5333a8b7837d39
Hiro Asari <asari.ruby@gmail.com>
2011-11-10 13:33:39 -0500

    Add benchmark

---
+++ bench_gamma.rb
@@ -0,0 +1,26 @@
+require 'ffi'
+require 'java'
+require 'benchmark'
+
+java_import 'Example'
+
+module Gamma
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
+  attach_function :tgamma, [:double], :double
+end
+
+include Benchmark
+
+bmbm(30) do |bm|
+  N = ARGV[1] || 1_000_000
+  bm.report("pure_java") {
+    N.times { x = Math.gamma(0.5) }
+  }
+  bm.report("JNI") {
+    N.times { x = Java::Example.gamma(0.5) }
+  }
+  bm.report("FFI") {
+    N.times { x = Gamma::tgamma(0.5) }
+  }
+end
