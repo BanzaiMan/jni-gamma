3402fcaab7296e4d121348c7e47eb149895c2579
Hiro Asari <asari.ruby@gmail.com>
2011-11-12 20:15:17 -0500

    benchmark can now run on MRI 1.9

--- bench_gamma.rb
+++ bench_gamma.rb
@@ -1,8 +1,9 @@
 require 'ffi'
-require 'java'
+if defined? JRUBY_VERSION
+  require 'java'
+  java_import 'Example'
+end
 require 'benchmark'
-
-java_import 'Example'
 
 module Gamma
   extend FFI::Library
@@ -14,12 +15,18 @@ include Benchmark
 
 bmbm(30) do |bm|
   N = ARGV[0].nil? ? 1_000_000 : ARGV[0].to_i
-  bm.report("pure_java") {
-    N.times { x = Math.gamma(0.5) }
-  }
-  bm.report("JNI") {
-    N.times { x = Java::Example.gamma(0.5) }
-  }
+  if defined? JRUBY_VERSION
+    bm.report("pure_java") {
+      N.times { x = Math.gamma(0.5) }
+    }
+    bm.report("JNI") {
+      N.times { x = Java::Example.gamma(0.5) }
+    }
+  elsif RUBY_VERSION > "1.9"
+    bm.report("native") {
+      N.times { x = Math.gamma(0.5) }
+    }
+  end
   bm.report("FFI") {
     N.times { x = Gamma::tgamma(0.5) }
   }
